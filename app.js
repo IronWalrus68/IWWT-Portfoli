4215e5ebbc5d8096a4cf8c3e4e5743d604d16ecd
Cameron <camsjobhunting@gmail.com>
2022-08-17 19:34:04 +0100

    change shape of pfp background

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const tween = KUTE.fromTo(
     '#blob1',
     { path: '#blob1' },
     { path: '#blob2' },
-    { repeat: 999, duration: 3000, yoyo: true }
+    { repeat: 999, duration: 8000, yoyo: true }
 ).start();
